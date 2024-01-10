d7cb2ca616dac57b207392befa9b31a913734956
Okonu <ianyakundi015@gmail.com>
2024-01-10 16:29:21 +0300

    product controller, user,role,product requests

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -11,7 +11,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email:rfc,dns|max:255|unique:users,email'.$this->user->id,
+            'password' => 'required|string|min:8|confirmed',
+            'roles' => 'required',
         ];
     }
 }
